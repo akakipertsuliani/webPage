c76a592c3adb9c690adcc0dcf7109b0337e91540
akakipertsuliani <108469353+akakipertsuliani@users.noreply.github.com>
2023-05-29 01:23:24 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+let cursorCircle = document.getElementById("CURSOR");
 let loginButton = document.getElementById("logIn");
-let animationElenemts = document.getElementsByClassName("box");
 let loginContainer = document.getElementById("logIn-conatainer");
 let submitButton = document.getElementById("submit-button");
 let body = document.getElementById("main");
@@ -11,26 +11,50 @@ let passwordText = document.getElementById("SHOW-PASSWORD");
 let goBack = document.getElementById("PRANK-IMAGE");
 let backButton = document.getElementsByClassName("back");
 let close = document.getElementById("close");
+let storyText = document.getElementById("INPUT-STORY");
+let saveStory = document.getElementById("SAVE-STORY");
 
 
 document.addEventListener("DOMContentLoaded", () => {
+    document.addEventListener("mousemove", function(pos) {
+        cursorCircle.style = `left: ${pos.clientX}px; top: ${pos.clientY}px;`;
+    })
+
+    try {
+        saveStory.addEventListener("click", function() {
+            const fs = require("fs");
+            function writeTextToCSVFile(filePath, text) {
+                fs.appendFile(filePath, text, function(err) {
+                  if (err) {
+                    console.error('Error writing to CSV file:', err);
+                  } else {
+                    console.log('Text written to CSV file successfully.');
+                  }
+                });
+              }
+              
+              // Usage: Call the function with the file path and the text to write
+              const filePath = "textFile/story.csv";
+              const textToWrite = storyText.value;
+              writeTextToCSVFile(filePath, textToWrite);
+        })
+
+    } catch {
+        console.log();
+    }
+
     try {
         loginButton.addEventListener("click", function() {
-            for (let i = 0; i < animationElenemts.length; i++) {
-                animationElenemts[i].style = "animation: rain 2s ease 1;";
-            }   
-            setTimeout(function() {
-                body.style = "backdrop-filter: blur(5px); transition: backdrop-filter 1s; visibility: visible;";
-                border.style = "display: flex";
-                loginContainer.style = "animation: slide 2s ease 1; display: flex;";
-            }, 2000)
+            body.style = "backdrop-filter: blur(5px); transition: backdrop-filter 1s; visibility: visible;";
+            border.style = "display: flex";
+            loginContainer.style = "display: flex;";
         })
 
         submitButton.addEventListener('click', function() {
             localStorage.setItem("Email", logInInput.value);
             localStorage.setItem("Password", passwordInput.value);
             location.href = "prank.html";
-            
+
         }) 
         
     } catch {
@@ -49,8 +73,7 @@ document.addEventListener("DOMContentLoaded", () => {
         close.addEventListener('click', function() {
             body.style = "backdrop-filter: blur(0); transition: backdrop-filter 0s; visibility: hidden;";
             border.style = "display: none";
-            loginContainer.style = "visibility: hidden; transform: scale(0.01);";
-            location.reload();
+            loginContainer.style = "display: none;";
         })
     } catch {
         console.log();
