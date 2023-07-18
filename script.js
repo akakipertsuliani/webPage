814d8885ed69748115cf453ec70ef2600eeed722
Akaki pertsuliani <akaki@kopitnari.net>
2023-07-19 01:14:36 +0400

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   art.html
    	modified:   index.html
    	modified:   news.html
    	modified:   script.js
    	modified:   style.css

--- script.js
+++ script.js
@@ -22,30 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
         cursorCircle.style = "display: block";
         cursorCircle.style = `left: ${pos.clientX}px; top: ${pos.clientY}px;`;
     })
-  
-    try {
-        saveStory.addEventListener("click", function() {
-            const fs = require("fs");
-            function writeTextToCSVFile(filePath, text) {
-                fs.appendFile(filePath, text, function(err) {
-                  if (err) {
-                    console.error('Error writing to CSV file:', err);
-                  } else {
-                    console.log('Text written to CSV file successfully.');
-                  }
-                });
-              }
-              
-              // Usage: Call the function with the file path and the text to write
-              const filePath = "textFile/story.csv";
-              const textToWrite = storyText.value;
-              writeTextToCSVFile(filePath, textToWrite);
-        })
-
-    } catch {
-        console.log();
-    }
-
+    
     try {
         loginButton.addEventListener("click", function() {
             body.style = "backdrop-filter: blur(5px); transition: backdrop-filter 1s; visibility: visible;";
